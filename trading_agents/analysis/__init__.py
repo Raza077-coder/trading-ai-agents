e66d11d368d3608f11cb15b314ef602befb69655
Ali Raza <raza8656825@gmail.com>
2026-08-18 20:17:28 +0500

    feat: add analysis package init

---
+++ trading_agents/analysis/__init__.py
@@ -0,0 +1,5 @@
+"""Analysis package: technical analysis agents and indicator math."""
+
+from trading_agents.analysis.technical_agent import TechnicalAnalysisAgent
+
+__all__ = ["TechnicalAnalysisAgent"]
